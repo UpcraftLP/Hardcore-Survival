f5c4bc8fdfa89311e623e3cb7dca9c478e29e670
Unknown <j-schirra@gmx.de>
2016-12-11 17:12:12 +0100

    updated update-url

--- src/main/java/mod/upcraftlp/hardcoresurvival/Reference.java
+++ src/main/java/mod/upcraftlp/hardcoresurvival/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 	public static final String VERSION = "1.5.0";
 	public static final String MODNAME = "Harcore Survival Mod";
 	public static final String MOD_ID = "hardcoresurvival";
-	public static final String INTERNAL_UPDATE_URL = "https://raw.githubusercontent.com/UpcraftLP/Hardcore-Survival/master/Version.txt";
+	public static final String INTERNAL_UPDATE_URL = "https://raw.githubusercontent.com/UpcraftLP/Hardcore-Survival/Hardcore-Survival-1.10/Version.txt";
 	public static final String UPDATE_URL = "http://mods.curse.com/mc-mods/minecraft/247029-hardcore-survival-mod";
 	public static final String MOD_URL = "http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/2704915";
 	public static final String CLIENT_PATH = "mod.upcraftlp." + MOD_ID + ".proxy.ClientProxy";
